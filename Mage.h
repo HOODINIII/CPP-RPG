ddbd987344f01108f4dc6f48714df1efe77b1c3b
HOOODINIII <muhammadumer091@gmail.com>
2022-05-08 21:31:57 +0100

    update

--- Mage.h
+++ Mage.h
@@ -6,11 +6,11 @@
 class Mage : public hp, public statblock
 {
 public:
-	static const hptype Hpgrowth = 100u;				//using static constant to only intentiate once no matter the number of knights.
-	static const stats basedmg = 50u;					// knights will spawn with hp of 100.
+	static const hptype Hpgrowth = 100u;				//using static constant to only intentiate once no matter the number of Mage.
+	static const stats basedmg = 50u;					// Mage will spawn with hp of 100.
 	static const stats baseintel = 50u;
 
-	Mage() : hp(Hpgrowth, Hpgrowth), statblock(basedmg, baseintel)	//each knight will start with similar stats
+	Mage() : hp(Hpgrowth, Hpgrowth), statblock(basedmg, baseintel)	//each Mage will start with similar stats
 	{
 
 	}
--- Preist.h
+++ Preist.h
@@ -6,11 +6,11 @@
 class Preist : public hp, public statblock
 {
 public:
-	static const hptype Hpgrowth = 100u;				//using static constant to only intentiate once no matter the number of knights.
-	static const stats basedmg = 10u;					// knights will spawn with hp of 100.
+	static const hptype Hpgrowth = 100u;				//using static constant to only intentiate once no matter the number of Preist.
+	static const stats basedmg = 10u;					// Preist will spawn with hp of 100.
 	static const stats baseintel = 70u;
 
-	Preist() : hp(Hpgrowth, Hpgrowth), statblock(basedmg, baseintel)	//each knight will start with similar stats
+	Preist() : hp(Hpgrowth, Hpgrowth), statblock(basedmg, baseintel)	//each Preist will start with similar stats
 	{
 
 	}

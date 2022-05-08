16f129a2c9db2c16b852a6e761e78724d9ccb8d7
HOOODINIII <muhammadumer091@gmail.com>
2022-05-08 21:47:05 +0100

    test

--- Main.cpp
+++ Main.cpp
@@ -12,5 +12,15 @@ int main()
 		<< "- MaxHP: " << knight1.getMaxhp() << '\n'
 		<< "- Damage: " << knight1.getdamage() << '\n'
 		<< "- Intelegence: " << knight1.getinteligence() << '\n';
+
+	std::cout << "Mage\n"
+		<< "- MaxHP: " << mage1.getMaxhp() << '\n'
+		<< "- Damage: " << mage1.getdamage() << '\n'
+		<< "- Intelegence: " << mage1.getinteligence() << '\n';
+
+	std::cout << "Preist\n"
+		<< "- MaxHP: " << preist1.getMaxhp() << '\n'
+		<< "- Damage: " << preist1.getdamage() << '\n'
+		<< "- Intelegence: " << preist1.getinteligence() << '\n';
 	return 0;
 }

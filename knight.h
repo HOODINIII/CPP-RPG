b86d81d18eb3c6e3ed1334653055f3585d4c2cec
HOOODINIII <muhammadumer091@gmail.com>
2022-05-08 21:11:52 +0100

    update

--- knight.h
+++ knight.h
@@ -8,8 +8,8 @@ class knight : public hp , public StatBlock
 {
 public:
 	static const hptype Hpgrowth = 100u;				//using static constant to only intentiate once no matter the number of knights.
-	static const stats damage = 50u;													// knights will also have hp growth of 100 unassinged.
-	static const stats inteligence = 50u;													// knights will also have hp growth of 100 unassinged.
+	static const stats damage = 50u;					// knights will spawn with hp of 100.
+	static const stats inteligence = 50u;													
 
 	knight() : hp(Hpgrowth, Hpgrowth)					//each knight will start with the
 	{

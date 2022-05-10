4799bcc05de4e05ed01d5de62b1664600e9f5c85
Muhammad Umar <muhammadumer091@gmail.com>
2022-05-10 11:06:03 +0100

    Revert "merge" (#4)

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,20 @@ int main()
 {
 	player p1(new knight());
 
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		std::cout
-			<< p1.getClassName()
-			<< "Knight" << " Level " << p1.getCurrentlevel() << '\n'
-			<< "- EXP:" << p1.getCurrentEXP() << "/" << p1.getxpforlevelup() << '\n'
-			<< "- Max HP:" << p1.getMaxHP << '\n'
+			<< p1.getClassname()
+			<< " Level " << p1.getCurrentlevel() << '\n'
+			<< "- EXP:" << p1.getcurrentEXP() << '/' << p1.getxpforlevelup() << '\n'
+			<< "- HP:" << p1.getCurrentHP() << '/' << p1.getMaxHP() << '\n'
 			<< "- Damage:" << p1.getdamage() << '\n'
-			<< "- Inteligence:" << p1.getinteligence() << '\n';
-			p1.EXPgain(100u);
+			<< "- Mana:" << p1.getmanapool() << '\n'
+			<< "- energy:" << p1.getenergy() << '\n';
+
+
+		p1.EXPgain(100u);
 	}
-	
+
 	return 0;
 }
